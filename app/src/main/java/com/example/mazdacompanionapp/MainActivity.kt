e0f8fc13f32c0d1b6e894a36b649f652cb90395c
Radovan Findra <findrar0@gmail.com>
2024-05-26 21:29:29 +0200

    first tests

--- app/src/main/java/com/example/mazdacompanionapp/MainActivity.kt
+++ app/src/main/java/com/example/mazdacompanionapp/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.mazdacompanionapp
 
+import android.app.Notification
+import android.bluetooth.BluetoothAdapter
 import android.os.Bundle
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -12,7 +18,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mazdacompanionapp.ui.theme.MazdaCompanionAppTheme
 
+val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+
 class MainActivity : ComponentActivity() {
+    private val notificationListener = NotificationListener()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +32,46 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Button(onClick = {
+                            val notificationCount = notificationListener.getNotificationCount()
+                            // Trigger sending of notification count here
+                        }) {
+                            Text("Send Notification Count")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+class NotificationListener : NotificationListenerService() {
+    private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+    private var notificationCount = 0
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MazdaCompanionAppTheme {
-        Greeting("Android")
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        super.onNotificationPosted(sbn)
+        notificationCount++
+    }
+
+    fun getNotificationCount(): Int {
+        return notificationCount
     }
 }
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        MazdaCompanionAppTheme {
+            Greeting("Android")
+        }
+    }
